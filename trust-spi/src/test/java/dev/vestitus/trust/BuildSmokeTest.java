285c5c6f2b31190e14b756c29c2407c9b3fe6bc8
lee-mcfaul2 <266500709+lee-mcfaul2@users.noreply.github.com>
2026-05-18 15:57:56 -0400

    build: add trust-spi maven module on jdk25

---
+++ trust-spi/src/test/java/dev/vestitus/trust/BuildSmokeTest.java
@@ -0,0 +1,11 @@
+package dev.vestitus.trust;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BuildSmokeTest {
+    @Test
+    void toolchainIsJava25() {
+        assertEquals(25, Runtime.version().feature());
+    }
+}
